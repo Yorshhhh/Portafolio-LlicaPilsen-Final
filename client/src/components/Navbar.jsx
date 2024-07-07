54b515b60d3cffcca92531ed736aed9c67eb526d
Yorshhhh <seinagi77@gmail.com>
2024-07-07 17:36:31 -0400

    commit navbar calendario

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -282,7 +282,7 @@ function Navbar() {
             href="#"
             className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
           >
-            Calendar
+            Calendario
           </a>
         </div>
       </div>
